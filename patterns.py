c53fa61161d84e23831be4cf3e38043a88dea503
Sid Saha <admin@Shameeks-MacBook-Pro-Trimian.local>
2018-02-26 20:17:51 +0530

    range percent ranges

--- patterns.py
+++ patterns.py
@@ -1,27 +1,31 @@
 # This file has functions to return candlestick patterns found in data
 
-def bullish_marubozu(data, range_percent = 1, shadow_points = 2):
+def bullish_marubozu(data, low_range_percent = 1, high_range_percent = 10, shadow_points = 2):
   res = []
   for d in data:
     if not d.get('open', None) or not d.get('high', None) or not d.get('low', None) or not d.get('close', None):
       continue
     if d.get('close') < d.get('open'): continue
     current_range_percent = ((d.get('close') - d.get('open')) * 100) / d.get('open')
-    if current_range_percent < range_percent: continue
+    if current_range_percent < low_range_percent: continue
+    if current_range_percent > high_range_percent: continue
     if d.get('low') < (d.get('open') - shadow_points): continue
     if d.get('high') > (d.get('close') + shadow_points): continue
+    d['stoploss'] = d.get('low')
     res.append(d)
   return res
 
-def bearish_marubozu(data, range_percent = 1, shadow_points = 2):
+def bearish_marubozu(data, low_range_percent = 1, high_range_percent = 10, shadow_points = 2):
   res = []
   for d in data:
     if not d.get('open', None) or not d.get('high', None) or not d.get('low', None) or not d.get('close', None):
       continue
     if d.get('close') > d.get('open'): continue
     current_range_percent = ((d.get('open') - d.get('close')) * 100) / d.get('close')
-    if current_range_percent < range_percent: continue
+    if current_range_percent < low_range_percent: continue
+    if current_range_percent > high_range_percent: continue
     if d.get('low') < (d.get('close') - shadow_points): continue
     if d.get('high') > (d.get('open') + shadow_points): continue
+    d['stoploss'] = d.get('high')
     res.append(d)
   return res
